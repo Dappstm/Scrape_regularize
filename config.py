d5c053e3303bc5cfb021918cf331a7f92c230598
Dappstm <anthonyata.ata3@gmail.com>
2025-08-29 12:08:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,15 @@ DEFAULT_OUT_DIR = Path("./out")
 DEFAULT_DB_PATH = Path("./data.sqlite")
 DEFAULT_DOWNLOAD_DIR = Path("./darfs")
 DEFAULT_USER_DATA = Path("./user_data")  # Playwright persistent profile
+DEFAULT_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    ),
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+}
 STORAGE_PATH = Path("./playwright_state.json")
 
 # Select browser
